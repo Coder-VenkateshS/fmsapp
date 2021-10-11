c5bf608af5f944e325fa7ba1fb5b38bfd3bdfc3d
iamneowise <wise@iamneo.ai>
2021-10-11 09:41:45 +0530

    Project scaffolding created

---
+++ src/main/java/com/neo/fms/fmsapp/FmsappApplication.java
@@ -0,0 +1,13 @@
+package com.neo.fms.fmsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmsappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neo/fms/fmsapp/FmsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo.fms.fmsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
